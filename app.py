bed7587b605f3270e6b9bc3df052e75c2e31253e
Pornilo <131370169+Pornilo@users.noreply.github.com>
2024-02-24 12:41:00 -0500

    Update app.py

--- app.py
+++ app.py
@@ -4,34 +4,40 @@ from openai import OpenAI
 client = OpenAI(api_key = st.secrets.OpenAIAPI.openai_api_key)
 
 if "messages" not in st.session_state:
-  st.session_state["messages"] = [{"role":"system","content":"Piensa como un ingeniero de Software"}]
+    st.session_state["messages"] = [
+  {"role": "system", "content": "Piensa como un Ingeniero de software"}
+]
 
-def comunicate():
-  messages = st.session_state["messages"]
-  user_message = {"role":"user","content":st.session_state["user input"]}
-  messages.append(user_message)
+def communicate():
+    messages = st.session_state["messages"]
+    user_message = {"role": "user", "content": st.session_state["user_input"]}
+    messages.append(user_message)
 
-  response = cliente.chat.completions.create(
-    model="gpt-3.5-turbo",
-    messages = messages
-  )
- 
-  bot_message = response.choices[0].messages
-  messages.append(bot_message)
 
-  st.session_state["user_input"] = ""
+    response = client.chat.completions.create(
+      model="gpt-3.5-turbo",
+      messages=messages
+    )
+    bot_message = response.choices[0].message
+    messages.append(bot_message)
 
-st.title("Desarrolador AI")
-st.write("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
 
-user_input = st.text_input("Por favor ingrese un mensaje aquí", key = "user_input", on_change = comunicate)
+    st.session_state["user_input"] = ""
+
+st.title ("Desarrollador AI")
+st.write ("Utilizando la API chatGPT, este chatbot ofrece capacidades conversacionales avanzadas.")
+
+
+user_input = st.text_input("por favor ingrese un mensaje aquí.", key = "user_input", on_change=communicate)
+
 
 if st.session_state["messages"]:
-  messages = st.session_state["messages"]
+    messages = st.session_state["messages"]
 
-  for message in reversed(messages[1:]):
-      if isinstance(message, dict):
-          speaker = "😎" if message["role"] == "user" else "🤖"
-          st.write (speaker + ": " + message["content"])
-      else:
-          st.write("🤖: " + message.content)
+
+    for message in reversed(messages[1:]):
+        if isinstance(message, dict):
+            speaker = "😎" if message["role"] == "user" else "🤖"
+            st.write (speaker + ": " + message["content"])
+        else:
+            st.write("🤖: " + message.content)
